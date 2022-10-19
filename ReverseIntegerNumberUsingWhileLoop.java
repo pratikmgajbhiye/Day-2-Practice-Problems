81ebebdb91c97a164a83c411cc6d405cf74906de
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-10-19 17:43:18 +0530

    Added: Find Reverse Integer Number using While Loop

---
+++ ReverseIntegerNumberUsingWhileLoop.java
@@ -0,0 +1,20 @@
+package com.BridgeLabz.Day2PracticeProblem;
+
+public class ReverseIntegerNumberUsingWhileLoop {
+	
+	public static void main(String[] args)   
+	{  
+	int number = 231, reverse = 0;
+	
+	System.out.println("The Number is: " +number);
+	
+	while(number != 0)   
+	{  
+	int remainder = number % 10;  
+	reverse = reverse * 10 + remainder;  
+	number = number/10;  
+	}  
+	System.out.println("The Reverse Number is: " +reverse);  
+	}  
+
+}
