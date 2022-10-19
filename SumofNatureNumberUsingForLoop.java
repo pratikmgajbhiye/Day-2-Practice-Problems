0812db28f41e5e66d0542b25c929889f5116bd38
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-10-19 17:48:53 +0530

    Added: Sum of n Natural Numbers using For Loop

---
+++ SumofNatureNumberUsingForLoop.java
@@ -0,0 +1,18 @@
+package com.BridgeLabz.Day2PracticeProblem;
+
+public class SumofNatureNumberUsingForLoop {
+	
+	public static void main(String[] args)   
+	{  
+	int i, num = 5, sum = 0;  
+	  
+	for(i = 1; i <= num; ++i)  
+	{  
+	  
+	sum = sum + i;  
+	}  
+	 
+	System.out.println("Sum of Natural Numbers is = " + sum);  
+	}
+
+}
