19bfd02bb7d5e76fdee73bb067dfc080077efd43
pratikmgajbhiye <gajbhiyepratik2@gmail.com>
2022-10-19 17:38:48 +0530

    Added: Find the Roots of Quadratic Equation

---
+++ Quadratic.java
@@ -0,0 +1,29 @@
+package com.BridgeLabz.Day2PracticeProblem;
+
+import java.util.Scanner;  
+public class Quadratic {
+	
+	public static void main(String[] Strings)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of a: ");
+		double a = sc.nextDouble();
+		System.out.print("Enter the value of b: ");
+		double b = sc.nextDouble();
+		System.out.print("Enter the value of c: ");
+		double c = sc.nextDouble();
+		double d= b * b - 4.0 * a * c;
+		if (d>0.0) {
+			double r1= (-b + Math.pow(d, 0.5)) / (2.0 * a);  
+			double r2= (-b - Math.pow(d, 0.5)) / (2.0 * a);  
+			System.out.println("The Roots are " + r1 + " and " + r2);  
+		}   
+		else if (d== 0.0) {
+			double r1 = -b / (2.0 * a);
+			System.out.println("The Root is " + r1);  
+			}   
+		else {
+			System.out.println("Roots Are Not Real.");
+			}
+		}
+}  
